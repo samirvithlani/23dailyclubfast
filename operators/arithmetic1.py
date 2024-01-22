9fb27490c09e1b72af6479320672cf22c8a09693
samirvithlani <samir.vithlani83955@gmail.com>
2024-01-22 17:59:30 +0530

    file

---
+++ operators/arithmetic1.py
@@ -0,0 +1,12 @@
+no1 = 5
+no2 = 5
+
+#+, -, *, /, %, //, **
+
+#ans = no1 / no2 #it returns float value
+ans = no1 // no2 #it returns int value #floor division
+print("ans = ", ans)
+
+#mul = no1 * no2
+mul = no1 ** no2 #exponent
+print("mul = ", mul)
