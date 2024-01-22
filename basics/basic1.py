483e81fc851fff6090e70c7630c4c60a84931721
samirvithlani <samir.vithlani83955@gmail.com>
2024-01-22 17:31:35 +0530

    basics

---
+++ basics/basic1.py
@@ -0,0 +1,32 @@
+# this is for single line comment...
+
+print("Hello World!",end="\n")
+print("Good Evening!")
+
+
+age = 20 #int
+print(age)
+print("age = ",age)
+print("Type of age = ",type(age)) #int
+
+pers = 78.89 #float
+print("pers = ",pers)
+print("Type of pers = ",type(pers)) #float
+
+
+name = "Ram" #string
+print("name = ",name)
+print("Type of name = ",type(name)) #string
+
+isPassed = True #boolean
+print("isPassed = ",isPassed)
+print("Type of isPassed = ",type(isPassed)) #boolean
+
+data = None #NoneType
+print("data = ",data)
+print("Type of data = ",type(data)) #None
+
+
+
+
+
---
+++ basics/basic2.py
@@ -0,0 +1,21 @@
+#input function..
+
+# no1 = input("Enter first number: ")
+# print("First number is: ", no1)
+# "101"
+# print("Type of no1 = ",type(no1))
+
+#explict type casting
+
+no1 = int(input("Enter first number: ")) 
+print("First number is: ", no1)
+print("Type of no1 = ",type(no1))
+
+pers = float(input("Enter pers: "))
+print("Pers is: ", pers)
+print("Type of pers = ",type(pers))
+
+
+isPassed = bool(input("Enter isPassed: "))
+print("isPassed is: ", isPassed)
+print("Type of isPassed = ",type(isPassed))
