d2eeba0ce855284e9faec1be66197a09b818c47d
samirvithlani <samir.vithlani83955@gmail.com>
2024-02-16 19:32:59 +0530

    list

---
+++ ds/list/listdemo1.py
@@ -0,0 +1,41 @@
+# data = [] # empty list
+# print(data)
+# print(type(data))
+
+data = [10,20,34,56,78,32,55,20]
+print(data)
+
+# l = len(data)
+# print("len",l)
+
+
+# for i in range(len(data)):
+#     print(data[i])
+
+# for i in data:
+#     print(i)
+
+
+print(data[0])
+data[0]=1000
+print(data)
+
+#grow
+data.append(3400)
+print(data)
+
+data.extend([34,56,800])
+print(data)
+
+#data.extend("amit")
+#data.extend(["amit"])
+print(data)
+
+data.insert(2,999)
+print(data)
+
+# data.remove(20)
+
+
+
+
---
+++ ds/list/listdemo2.py
@@ -0,0 +1,23 @@
+data =["java",'python','c',"java",'c++']
+print(data)
+#removedELm = data.pop() #it will remove last element
+# removedELm = data.pop(2) 
+# print("removing...",removedELm)
+
+
+if data.count("python1")>0:
+    data.remove("python1")
+else:
+    print("not found")    
+    
+print(data)
+
+
+cnt = data.count('java1')
+print("count",cnt)
+
+
+
+
+
+
