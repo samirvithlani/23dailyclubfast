1475af3afec516f8a776362779c3b9adb7213f89
samirvithlani <samir.vithlani83955@gmail.com>
2024-04-26 17:27:57 +0530

    update scrore board

--- functions/filter/scoreboard.py
+++ functions/filter/scoreboard.py
@@ -1,12 +1,12 @@
 scorecard = [
-    {"player": "Kohli", "dismissal": "c Abdul Samad b Jaydev Unadkat", "runs": 51, "balls": 43, "strike_rate": 118.60},
-    {"player": "du Plessis (c)", "dismissal": "c Markram b T Natarajan", "runs": 25, "balls": 12, "strike_rate": 208.33},
-    {"player": "Will Jacks", "dismissal": "b Markande", "runs": 6, "balls": 9, "strike_rate": 66.67},
-    {"player": "Rajat Patidar", "dismissal": "c Abdul Samad b Jaydev Unadkat", "runs": 50, "balls": 20, "strike_rate": 250.00},
-    {"player": "Green", "dismissal": "not out", "runs": 37, "balls": 20, "strike_rate": 185.00},
-    {"player": "Lomror", "dismissal": "c Cummins b Jaydev Unadkat", "runs": 74, "balls": 40, "strike_rate": 175.00},
-    {"player": "Karthik (wk)", "dismissal": "c Abdul Samad b Cummins", "runs": 11, "balls": 6, "strike_rate": 183.33},
-    {"player": "Swapnil Singh", "dismissal": "c Abhishek Sharma b T Natarajan", "runs": 12, "balls": 6, "strike_rate": 200.00}
+    {"batter": "Kohli", "dismissal": "c Abdul Samad b Jaydev Unadkat", "runs": 51, "4s": 4, "6s": 1, "strike_rate": 118.60},
+    {"batter": "du Plessis (c)", "dismissal": "c Markram b T Natarajan", "runs": 25, "4s": 1, "6s": 0, "strike_rate": 208.33},
+    {"batter": "Will Jacks", "dismissal": "b Markande", "runs": 6, "4s": 0, "6s": 0, "strike_rate": 66.67},
+    {"batter": "Rajat Patidar", "dismissal": "c Abdul Samad b Jaydev Unadkat", "runs": 50, "4s": 2, "6s": 5, "strike_rate": 250.00},
+    {"batter": "Green", "dismissal": "not out", "runs": 37, "4s": 2, "6s": 0, "strike_rate": 185.00},
+    {"batter": "Lomror", "dismissal": "c Cummins b Jaydev Unadkat", "runs": 74, "4s": 10, "6s": 1, "strike_rate": 175.00},
+    {"batter": "Karthik (wk)", "dismissal": "c Abdul Samad b Cummins", "runs": 11, "4s": 1, "6s": 0, "strike_rate": 183.33},
+    {"batter": "Swapnil", "dismissal": None, "runs": None, "4s": None, "6s": None, "strike_rate": None}
 ]
 
 print(scorecard)
