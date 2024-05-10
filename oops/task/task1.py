4e005b437a2de193f4bc15f9c93b0042795b36e9
samirvithlani <samir.vithlani83955@gmail.com>
2024-05-10 17:21:56 +0530

    product app

---
+++ oops/task/task1.py
@@ -0,0 +1,28 @@
+#add product
+#view products
+#delete product
+#update product
+
+#add to cart
+#checkout
+
+class Amazone:
+    
+    def __init__(self):
+        pass
+    def addProduct():
+        pass
+    def viewProduct():
+        pass
+    def deleteProduct(pid):
+        pass
+    def updateProduct(pid):
+        pass
+    def addToCart(pid):
+        pass
+  
+    def checkout():
+        pass    
+        
+    
+        
